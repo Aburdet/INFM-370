4005f600ae4be43521b700a42668a594106d16e6
Aburdet <aburd899@gmail.com>
2023-04-22 10:30:35 -0500

    Commit 2

--- Project-13/src/com/company/MainUF.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class MainUF {
-
-    public static void main(String[] args) {
-        UF arr = new UF();
-        arr.connect(0,1);
-    }
-}
--- Project-4/src/com/company/Queue3.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class Queue3 {
-    int[] queue = new int[7];
-    int index = 0;
-    public void enqueue(int value) {
-
-    }
-}
